bc31066696acd703a9d0333220c7f35f0a1a93a4
duffigoogle <calebdduff@gmail.com>
2022-11-02 06:57:18 +0000

    fix file requirements

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,9 +4,8 @@ function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit
 """
 
-import sys
-import requests
 import json
+import requests
 
 
 def top_ten(subreddit):
